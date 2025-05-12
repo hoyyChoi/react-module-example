e149f54a754eb42d9a79f367cd13111a72a494c7
hoyyChoi <hoyeon8165@gmail.com>
2025-05-12 13:46:39 +0900

    feat: HashRouter 적용

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import App from "../App";
 import HookPage from "./pages/HookPage";
 import ModalPage from "./pages/ModalPage";
 
 const Router = () => {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/modal' element={<ModalPage />} />
         <Route path='/hook' element={<HookPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
